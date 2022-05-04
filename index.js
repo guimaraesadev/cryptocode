2a49e227a896c4fd94f58aabad2dd9e9e05b7580
Sabrina S. Guimarães <guimaraesadev@gmail.com>
2022-05-03 22:33:39 -0300

    Summary (requered)

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-
-/*
-O que meu arquivo JavaScript vai fazer: 
-1. Incrementar a opção "adicione seu passo" na CIFRA CÉSAR através do ID "ocultar incremento", ou seja, através do DOM eu vou criar a opção de incremendo, e só aparecer com ela quando o usuário clicar na opção cifra césar 
-
-BOTÃO RADIO => criptografar e depois inverter o comando se o usuário solicitar; 
-
-2. Criar variável para armazenar cada ID e cada elemento selecionando através do .querySelector
-3. Meu arquivo JS vai ouvir o que está sendo passando através do DOM com o addEventListener // Principalmente nos botões; => 
-4. Adicionar funcionalidade ao botão "CODE here_" para ele OUVIR quando o usuário clicar e codificar de acordo com a base escolhida (Base64 ou Cifra César)
-
-*/
-    
+// criando minhas variáveis // selecionando elas através do document.querySelector OU document.getElementById 
+// qual método de codificação o usuário irá escolher => Base64 ou CifraCésar
 var selecioneMetodo = document.querySelector('#selecione') 
+// incremento = botão que o usuário digita os "PASSOS => CifraCésar" => Na função: ocultar e só mostrar após o click
 var ocultarIncremento = document.querySelector('#ocultarIncremento') 
 var code = document.querySelector('#codificar') 
 var decode = document.querySelector('#decodificar') 
